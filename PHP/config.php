5be47e198a24e1df325c54a99610b36fef0c8d38
paranroman <farhannrahmann@gmail.com>
2024-10-12 17:29:49 +0700

    login database rework

--- PHP/config.php
+++ PHP/config.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php 
 $host = "localhost";
 $dbuser = "postgres";
@@ -16,8 +8,3 @@ $dbname = "Database_Pemain_PairPlay";
 $conn = pg_connect("host=$host dbname=$dbname user=$dbuser password=$dbpass");
 
 ?>
-</body>
-</html>
-
-
-
