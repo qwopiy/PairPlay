39b9f7d154d0a04640254d73cabbe6437f628e95
paranroman <farhannrahmann@gmail.com>
2024-10-12 17:25:28 +0700

    registration database

---
+++ PHP/config.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+$host = "localhost";
+$dbuser = "postgres";
+$dbpass = "1Wak_Izul99";
+$dbname = "Database_Pemain_PairPlay";
+
+
+$conn = pg_connect("host=$host dbname=$dbname user=$dbuser password=$dbpass");
+
+?>
+</body>
+</html>
+
+
+
