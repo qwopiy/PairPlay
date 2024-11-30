4955995aa86dc8625a205735146b79fa80ecba9d
Bintang Khalik <bintangkhaliqq@gmail.com>
2024-11-30 20:06:48 +0700

    fix achievement lagi

--- Signup and Login/verify/profileFunction.php
+++ Signup and Login/verify/profileFunction.php
@@ -6,14 +6,12 @@
 
         $data = file_get_contents("php://input");
         $data = json_decode($data, true);
-        // foreach ($data as &$value) {
-        //     $value = str_replace('http://localhost/PairPlay/','', $value);
-        // }
-        // unset($value);
-        // $data = json_encode($data, true);
-
-        $death = $data['death'];
-        $check = database_run("update pemain SET progress = $death WHERE id = 5");
-        echo $data['death'];
+        foreach ($data as &$value) {
+            $value = str_replace('http://localhost/PairPlay/','', $value);
+        }
+        unset($value);
+        $data = json_encode($data, true);
+        $check = database_run("update pemain SET achievement = '$data' WHERE id = '$id'");
+        echo $data;
     }
 ?>
